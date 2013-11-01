64e766371b0f5ff57a8c27ba54269543c190e519
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-11-01 19:28:58 +0530

    db.clj: connect to MongoHQ instead of localhost

--- src/crossfire/db.clj
+++ src/crossfire/db.clj
@@ -3,7 +3,8 @@
             [monger.collection :as coll])
   (:import [org.bson.types ObjectId]))
 
-(mg/connect! )
+;; (mg/connect! )
+(mg/connect-via-uri! (System/genenv "MONGOHQ_URL"))
 (def db (mg/set-db! (mg/get-db "kcl")))
 
 (defn insert [doc]
