9c71f06372035726ea5aba4d18eba200be04f20a
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-11-03 00:07:00 +0530

    Update youtube link for KCL in footer.

--- src/crossfire/views/footer.clj
+++ src/crossfire/views/footer.clj
@@ -8,7 +8,7 @@
   <div class=\"col-md-2\"><h4><p align=\"center\" class=\"text-muted credit\">KCL</h4></p></div>
   <div class=\"col-md-5\"><p align=\"right\" class=\"text-muted credit\">
 <a class=\"text-muted credit\" href=\"https://www.facebook.com/KgpCyberLeague\"><i class=\"fa fa-facebook-square fa-2x\"></i></a>
-<a class=\"text-muted credit\" href=\"youtube.com\"><i class=\"fa fa-youtube-square fa-2x\"></i></a>
+<a class=\"text-muted credit\" href=\"http://www.youtube.com/user/kgpcyberleague\"><i class=\"fa fa-youtube-square fa-2x\"></i></a>
 <a class=\"text-muted credit\" href=\"mailto:kcl.iitkgp@gmail.com\"><i class=\"fa fa-envelope fa-2x\"></i></a>
 </p></div>
 <!--
